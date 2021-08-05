9ebd970a4a92850b5ecbd78d2d705c538b1ef1b6
bophancungung <bophancungung@gmail.com>
2021-08-05 13:40:53 +0700

    Fetching data with Javascript

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Get Quote From API
+async function getQuote() {
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json()
+    console.log(data);
+  } catch (error) {
+    getQuote();
+    console.log('whoops, no quote', error);
+  }
+}
+
+// On Load
+getQuote();
+
 /*
   Setting up our project
 github
@@ -10,6 +27,9 @@ HTML elements
 Styling
 
   Create beautiful button
+
+  fetching data with Javascript
+https://forismatic.com/en/api/
 
 */
 
