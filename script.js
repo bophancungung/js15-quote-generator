2981a61344b745c1a91f0dc2ed2e029d134087f3
bophancungung <bophancungung@gmail.com>
2021-08-05 08:32:00 +0700

    Setting up our project

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// alert('helllllooooo');
+/*
+github
+heropatterns.com --> Circuit Board --> copy Generated CSS Code
+https://fonts.google.com/specimen/Montserrat?query=montserrat => @IMPORT
+
+*/
+
