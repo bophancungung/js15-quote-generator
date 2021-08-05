6a604af76d5ae1bde776917814c5a0e04995d4bc
bophancungung <bophancungung@gmail.com>
2021-08-05 10:18:36 +0700

    Styling and HTML elements

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 /*
+  Setting up our project
 github
 heropatterns.com --> Circuit Board --> copy Generated CSS Code
 https://fonts.google.com/specimen/Montserrat?query=montserrat => @IMPORT
+
+  Styling and HTML elements
+HTML elements
+? button tag using fab has class or title will error
+Styling
+
 
 */
 
